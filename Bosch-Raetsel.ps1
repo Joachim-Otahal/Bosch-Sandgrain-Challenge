4347d44411649b53222902c81026931f7e3e177d
Joachim Otahal <jou@gmx.net>
2022-04-12 00:18:48 +0200

    Generalized

--- Bosch-Raetsel.ps1
+++ Bosch-Raetsel.ps1
@@ -3,23 +3,48 @@ Bosh Rätsel https://www.facebook.com/BoschKarriere/photos/a.210421252381646/496
 Unklare Definition:
 Was ist mit Sandkörnern die über den Rand gehen? Bei mir fallen die vom Brett und sind verloren.
 Powershell Variante, einfach nur weil ich die letzten 2,5 Jahre fast nur Powershell gemacht habe.
+
+Hinweis: Ich ignoriere alle "über die Boardgrenzen Zugriff" Fehler :D.
+
 #>
 
+#### Boardgröße
+
+$BoardXSize = [int]100
+$BoardYSize = [int]100
+$Board = New-Object 'object[,]' $BoardXSize,$BoardYSize
+
+#### Startkorn
+
+$Board[48,49] = [int]1
+
+#### Droppoint
+
+$DropX = [int]49
+$DropY = [int]49
+
+#### Interationen aka "Sandkörner"
+
+$Iterationen = [int]20000
+
+#### CSV vom Ergebnis
+$OutFile=".\Bosch-Raetsel-" + $StartDate.ToString("yyyy-MM-dd_HH-mm-ss") + ".csv"
+
+#### Init
+
 $StartDate = Get-Date
-$OutFile="c:\tmp\Bosch-Raetsel-" + $StartDate.ToString("yyyy-MM-dd_HH-mm-ss") + ".csv"
-$Board = New-Object 'object[,]' 100,100
-for ($y=0;$y -lt 100;$y++) {
-    for ($x=0;$x -lt 100;$x++) {
+for ($y=0;$y -lt $BoardYSize;$y++) {
+    for ($x=0;$x -lt $BoardXSize;$x++) {
         $Board[$x,$y] = [int]0
     }
 }
-$Board[48,49] = [int]1
 
-for ($i=0;$i -lt 20000;$i++) {
-    $Board[49,49]++
+for ($i=0;$i -lt $Iterationen;$i++) {
+    #### Einzelnes Sandkorn
+    $Board[$DropX,$DropY]++
     while ($Board.Where({$_ -ge 4})) {
-        for ($y=0;$y -lt 100;$y++) {
-            for ($x=0;$x -lt 100;$x++) {
+        for ($y=0;$y -lt $BoardYSize;$y++) {
+            for ($x=0;$x -lt $BoardXSize;$x++) {
                 if ($Board[$x,$y] -ge 4) {
                     $Board[($x-1),$y]++
                     $Board[$x,($y-1)]++
@@ -30,21 +55,23 @@ for ($i=0;$i -lt 20000;$i++) {
             }
         }
     }
-    if ($i/100 -eq [Math]::Floor($i/100)) { Write-Host "Iteration $i" }
+    #### Statusupdate
+    if ($i/100 -eq [Math]::Floor($i/100)) { Write-Host "Iteration $i von $Iterationen" }
 }
 $EndDate = Get-Date
-[math]::Ceiling(($EndDate - $StartDate).TotalSeconds)
+Write-Host "$([math]::Ceiling(($EndDate - $StartDate).TotalSeconds)) Sekunden benötigt"
 
+# Zählen und CSV erstellen
 $count0=$count1=$count2=$count3=[int]0
 Out-File -FilePath $OutFile -NoNewline -Encoding utf8 -InputObject '"y\x";'
-for ($x=0;$x -lt 99;$x++) {
-    Out-File -FilePath $OutFile -Append -NoNewline -Encoding utf8 -InputObject "$(($x-49).ToString());"
+for ($x=0;$x -lt $BoardXSize-1;$x++) {
+    Out-File -FilePath $OutFile -Append -NoNewline -Encoding utf8 -InputObject "$(($x-[Math]::Floor($BoardXSize/2)).ToString());"
 }
-Out-File -FilePath $OutFile -Append -Encoding utf8 -InputObject "$(($x-49).ToString())"
-for ($y=0;$y -lt 100;$y++) {
-    Out-File -FilePath $OutFile -Append -NoNewline -Encoding utf8 -InputObject "$(($y-49).ToString());"
-    for ($x=0;$x -lt 100;$x++) {
-        if ($x -ne 99) {
+Out-File -FilePath $OutFile -Append -Encoding utf8 -InputObject "$(($x-[Math]::($BoardXSize/2)).ToString())"
+for ($y=0;$y -lt $BoardXSize;$y++) {
+    Out-File -FilePath $OutFile -Append -NoNewline -Encoding utf8 -InputObject "$(($x-[Math]::Floor($BoardYSize/2)).ToString());"
+    for ($x=0;$x -lt $BoardXSize;$x++) {
+        if ($x -ne ($BoardXSize-1)) {
             Out-File -FilePath $OutFile -Append -NoNewline -Encoding utf8 -InputObject "$($Board[$x,$y].ToString());"
         } else {
             Out-File -FilePath $OutFile -Append -Encoding utf8 -InputObject "$($Board[$x,$y].ToString())"
@@ -63,3 +90,5 @@ Write-Host "0: $count0"
 Write-Host "1: $count1"
 Write-Host "2: $count2"
 Write-Host "3: $count3"
+Write-Host "Übrige Sandkörner: $($count3*3+$count2*2+$count1)"
+Write-Host "Verlorene Sandkörner: $($Iterationen-$count3*3-$count2*2-$count1)"
